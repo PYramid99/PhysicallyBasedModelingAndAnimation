d6702fdf5e90fda95d66b737a372df5c3c57d909
PYramid99 <yuanpeng322@foxmail.com>
2025-06-01 00:21:36 +0800

    [Update] Project Structure

--- Assets/PBMA/Chapter2/FallBodySolution.cs
+++ Assets/PBMA/Chapter2/FallBodySolution.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PBMA.Core;
 
-namespace Chapter2
+namespace PBMA.Chapter2
 {
     public class FallBodySolution : SingletonMonoBehaviour<FallBodySolution>
     {
--- Assets/PBMA/Chapter2/FallBodyWind.cs
+++ Assets/PBMA/Chapter2/FallBodyWind.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Chapter2
+namespace PBMA.Chapter2
 {
     public class FallBodyWind : MonoBehaviour
     {
